a48011835dbb05c29fa5474f317ac6347cee7463
Annabel Tukiman <77195444+Annabel03@users.noreply.github.com>
2025-07-04 00:16:25 +0800

    Edit chat ui

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,8 +1,9 @@
 import KeyboardAvoidingViewContainer from "@/components/KeyboardAvoidingViewContainer";
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import { BACKEND_API_HOST } from "@env";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Keyboard, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, Keyboard, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 type Message = {
     sender: "user" | "bot";
@@ -13,6 +14,8 @@ export default function Chat() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState<string>("")
   const [textInputHeight, setTextInputHeight] = useState(60)
+
+  const router = useRouter();
 
   const handleContentSizeChange = (event: {
     nativeEvent: { contentSize: { height: number }};
@@ -62,6 +65,26 @@ export default function Chat() {
   return (
     <KeyboardAvoidingViewContainer>
       <View style={styles.container}>
+        <View style={styles.headerContainer}>
+            <TouchableOpacity
+                style={styles.backButton}
+                onPress={() => router.push("./(tabs)/home")}
+            >
+                <Ionicons name="close" size={30} color="#FFFFFF" />
+            </TouchableOpacity>
+
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>Pet Name</Text>
+            </View>
+
+            <TouchableOpacity 
+              style={styles.petPhotoContainer}
+              onPress={() => console.log("Pet profile photo tapped")} // TO-DO: Link to chat history
+            >
+              <Image source={require("../assets/images/cat.png")} style={styles.petPhoto}/>
+            </TouchableOpacity>
+        </View>
+
         <ScrollView
           style={styles.messagesContainer}
           contentContainerStyle={{padding: 10}}
@@ -96,13 +119,24 @@ export default function Chat() {
             onContentSizeChange={ handleContentSizeChange }
             onSubmitEditing={ handleSubmit }
           />
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={ handleSubmit }
-          >
-            <IconSymbol size={20} name="paperplane.fill" color="#060256" />
-          </TouchableOpacity>
+          
+          {
+            input.trim() ? (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={ handleSubmit }
+              >
+                <Ionicons size={20} name="send" color="#060256" />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => console.log("Mic button tapped")} // TO-DO: Handle speech-to-text
+              >
+                <Ionicons size={30} name="mic" color="#060256" />
+              </TouchableOpacity>
+            )
+          }
         </View>
         
       </View>
@@ -114,6 +148,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#EA6F1D",
+  },
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#060256",
+    height: 100,
+    paddingHorizontal: 10,
+    paddingTop: 30,
+  },
+  backButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 50,
+  },
+  titleContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+  },
+  petPhotoContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 60,
+  },
+  petPhoto: {
+    width: 50,
+    height: 50,
   },
   messagesContainer: {
     flex: 1,
@@ -149,8 +216,6 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "#FFFFFF",
-    paddingVertical: 12,
-    paddingHorizontal: 15,
     borderRadius: 24,
     height: 40,
     width: 40,
--- components/KeyboardAvoidingViewContainer.tsx
+++ components/KeyboardAvoidingViewContainer.tsx
@@ -16,7 +16,7 @@ export default function KeyboardAvoidingViewContainer({
         <KeyboardAvoidingView
             style={{flex: 1}}
             behavior="padding"
-            keyboardVerticalOffset={73}
+            keyboardVerticalOffset={-18}
         >
         { children }
         </KeyboardAvoidingView>
